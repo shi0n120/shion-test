41bf3046e6705439806db70e9e0a19c8c8e7f8ff
shi0n120 <makoto.yoshida0120@gmail.com>
2026-09-06 00:01:42 +0900

    sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='shion-v01';const A=['./','index.html','style.css','app.js','manifest.json','assets/shion.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
